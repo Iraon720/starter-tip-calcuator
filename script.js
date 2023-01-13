6b5b2542bf3625a311005f0d7a39e02344276c30
Iraon720 <iraon3493@gmail.com>
2023-01-13 01:54:09 -0500

    People Value

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
 // Note: The elements needed have been queried for you here
 // ** Query elements
 const bill = document.getElementById('bill');
-const gratuityBtns = document.querySelectorAll('.gratuity');
+const gratuityBtn = document.querySelectorAll('.gratuity');
 const customGratuity = document.getElementById('custom-gratuity');
 const people = document.getElementById('people');
 const splitTip = document.getElementById('split-tip');
@@ -22,3 +22,52 @@ const splitTotal = document.getElementById('split-total');
 const resetBtn = document.getElementById('reset');
 
 // ** Your work goes below here
+let billValue = 0;
+let peopleValue = 1;
+let tipValue = 0.15;
+
+bill.addEventListener{'input',validateBill};
+
+function validateBill(){
+if(InputDeviceInfo.value.includes(',')){
+  input.value.replace(',','.')
+}
+billValue = parseFloat(input.value);
+calculate()
+console.log(billValue);
+}
+customGratuity.addEventListener('input', customGratuity);
+people.addEventListener('input', peopleValue);
+resetBtn.addEventListener('click', handleReset);
+
+gratuityBtn.forEach(btn => {
+  btn.addEventListener('click',handleClick);
+});
+
+function handleClick(event){
+  gratuityBtn.forEach(btn => {
+    btn.classList.remove('active');
+    if(event.target.innerHTML === btn.innerHTML){
+      btn.classList.add('active');
+      tipValue = parseFloat(btn.innerHTML)/100
+      console.log(tipValue);
+
+    })
+    custom-gratuity.value='';
+    calculate()
+}
+function customTipValue(){
+  tipValue = parseFloat(custom-gratuity.value/100);
+  button.forEach(btn => {
+btn.classlist.remove('active');
+  })
+  if(custom-gratuity.value !== 0){
+    calculate();
+  }
+  console.log(tipValue);
+}
+
+
+
+
+
